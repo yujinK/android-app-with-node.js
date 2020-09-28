5bd01d8dc9804d4abbf651b8afd2b9406f1ba4e1
yujinK <yujinkim.dev@gmail.com>
2020-09-29 00:47:49 +0900

    finish!

---
+++ Android App/app/src/main/java/com/yujin/nodejs/LoginResult.java
@@ -0,0 +1,14 @@
+package com.yujin.nodejs;
+
+public class LoginResult {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
